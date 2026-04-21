fd0a1449831e434596973f2c9e9b2b07070dfa94
himandhi <himandhiow191@gmail.com>
2026-04-22 00:23:21 +0530

    New modules and files are created, Error fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ExpenseModule } from './expense/expense.module';
+import { IncomeModule } from './income/income.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, ExpenseModule, IncomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
